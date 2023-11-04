80384e3fba6ed5b4132a5b596b8f7da6fdd6761c
Gaurav <gauravrajahela884@gmail.com>
2023-11-04 15:02:35 +0530

    major changes

--- app.js
+++ app.js
@@ -1,29 +1,56 @@
 let btn = document.querySelector("button");
 let input = document.querySelector("input");
+let ul = document.querySelector("ul");
 
 btn.addEventListener("click", () => {
   if (input.value == "") {
     alert("Please enter task!");
   } else {
-    let ul = document.querySelector("ul");
     let li = document.createElement("li");
     ul.appendChild(li);
     li.innerText = input.value;
-    input.value = "";
 
     let del = document.createElement("span");
     del.innerHTML = "\u00d7";
     li.appendChild(del);
-    // li.insertAdjacentElement("afterend",del);
+    input.value = "";
+    getData();
   }
 });
 
-let ul = document.querySelector("ul");
 ul.addEventListener("click", (e) => {
   if (e.target.tagName === "LI") {
     e.target.classList.toggle("checked");
   } else if (e.target.tagName === "SPAN") {
-    e.stopPropagation();
     e.target.parentElement.remove();
   }
+  getData();
 });
+
+
+const getData=()=>{
+  let inputData= ul.innerHTML;
+  localStorage.setItem('data',inputData);
+};
+
+window.addEventListener('load',()=>{
+  let inputData= localStorage.getItem('data');
+  ul.innerHTML=inputData;
+});
+
+
+let icon=document.querySelector("i");
+let body=document.querySelector("body");
+
+icon.addEventListener("click",()=>{
+ icon.classList.toggle("fa-sun")
+ icon.classList.toggle("fa-moon")
+ if(icon.classList.contains('fa-moon')){
+    body.style.background="black";
+    body.style.color="grey";
+    body.style.transition='1s'
+ }else{
+    body.style.background="white";
+    body.style.color="black";
+ }
+})
